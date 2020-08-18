0fe535495297d321aa010a0e8fbd3d2803459ac7
zachee <zacheeishimwe@gmail.com>
2020-08-18 18:55:54 +0200

    Updates

---
+++ precache-manifest.36574b434317b7d4b8de199d156a2774.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d1afdc215eabf18f3797329bd51bec7",
+    "url": "/alynsano/index.html"
+  },
+  {
+    "revision": "fbc88749318af7d63193",
+    "url": "/alynsano/static/css/2.0e94b0d1.chunk.css"
+  },
+  {
+    "revision": "1f3876987db0f3fee224",
+    "url": "/alynsano/static/css/main.5d954173.chunk.css"
+  },
+  {
+    "revision": "fbc88749318af7d63193",
+    "url": "/alynsano/static/js/2.5cfb6b2a.chunk.js"
+  },
+  {
+    "revision": "8405fc7db03aa55859ceaaa8183fa901",
+    "url": "/alynsano/static/js/2.5cfb6b2a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1f3876987db0f3fee224",
+    "url": "/alynsano/static/js/main.8e3f6cd2.chunk.js"
+  },
+  {
+    "revision": "91ddd3b25d40ac133b9b",
+    "url": "/alynsano/static/js/runtime-main.d763ccc1.js"
+  },
+  {
+    "revision": "086c693facaf1926845b3c798d81decc",
+    "url": "/alynsano/static/media/LibreCaslonDisplay-Regular.086c693f.ttf"
+  },
+  {
+    "revision": "f6238deb7f40a7a03134c11fb63ad387",
+    "url": "/alynsano/static/media/OpenSans-Italic.f6238deb.ttf"
+  },
+  {
+    "revision": "ba5cde21eeea0d57ab7efefc99596cce",
+    "url": "/alynsano/static/media/OpenSans-SemiBold.ba5cde21.ttf"
+  },
+  {
+    "revision": "20e1565dd5c9e47bd241d99c0dd55ad1",
+    "url": "/alynsano/static/media/PlayfairDisplay-VariableFont_wght.20e1565d.ttf"
+  }
+]);
