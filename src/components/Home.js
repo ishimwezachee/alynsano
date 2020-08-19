5c1bae418323ba05620f31fa6784a9ed94922a4f
zachee <zacheeishimwe@gmail.com>
2020-08-19 15:43:06 +0200

    add play button

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,5 @@
 import React,{useEffect} from 'react';
+import { SocialMediaIconsReact } from 'social-media-icons-react';
 import Aos from 'aos';
 import 'aos/dist/aos.css';
 import './Home.css';
@@ -12,6 +13,9 @@ const Home = ({id}) => {
 			<div className="image"  data-aos="fade-right">
 				<img src="https://www.ktpress.rw/wp-content/uploads/2020/03/Alyn-Sano.jpg" alt="sano"/>
 			</div>
+			<div id="iconYoutbe">
+							<SocialMediaIconsReact icon="youtube-play" iconColor="#fff" url="https://www.youtube.com/channel/UCXLjm0fpBTA71gjdUU6D3jw" iconSize={10} backgroundColor="#000" />
+						</div>
 			<div className="imageText" data-aos="fade-left">
 				<h1 className="headName">ALYN</h1>
 				<h1 className="headName">SANO</h1>
